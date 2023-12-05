480268aac8cd59cf67d190c4c33822dadfdee829
marcocentonze <marco.centonze27@gmail.com>
2023-12-05 19:03:04 +0100

    add AppFooter component

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import { createApp } from 'vue'
-import './style.css'
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-library.add()
-import App from './App.vue'
+import { createApp } from "vue";
+import "../src/assets/scss/app.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).component('font-awesome-icon',FontAwesomeIcon).mount('#app')
+import * as bootstrap from "bootstrap";
+
+import {
+  faAtlassian,
+  faFacebookF,
+  faTwitter,
+  faYoutube,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faAtlassian,
+  faFacebookF,
+  faTwitter,
+  faYoutube,
+  faInstagram,
+);
+import App from "./App.vue";
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
