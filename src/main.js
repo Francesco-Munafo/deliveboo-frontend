e5f1cadf6b18ee8ddf41c4b3e10830d28533c28b
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-12-06 12:42:54 +0100

    create navbar and jumbo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import '../src/assets/scss/app.scss'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 library.add()
+import * as bootstrap from "bootstrap";
 import App from './App.vue'
 
 createApp(App).component('font-awesome-icon',FontAwesomeIcon).mount('#app')
