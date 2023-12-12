9f3755f3c22bee06a97ddd9dc5df9fe7e2668163
Francesco-Munafo <acquamarea1@gmail.com>
2023-12-12 16:49:05 +0100

    Add new filter logic

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -13,116 +13,45 @@ export default {
       selectedTypes: [],
       url_restaurant: "/api/restaurants",
       selectedRestaurants: [],
-      pageLinks: null,
+      links: [],
       currentPage: 1,
+      maxPages: null,
     };
   },
   methods: {
-    getType(url) {
+    /**
+     * GetType
+     *
+     * This function gets all restaurant types and adds them into an "copied" array
+     *
+     */
+
+    fetchTypes() {
       axios
-        .get(url)
+        .get(this.url + this.url_type)
         .then((response) => {
-          let tempTypes = response.data.results;
-          tempTypes = tempTypes.map((type) => ({ ...type, checked: false }));
-          this.types = tempTypes;
-          console.log("types:", this.types);
+          this.types = response.data.results;
         })
         .catch((err) => {
           console.error(err);
         });
     },
-    async fetchData() {
-      try {
-        const response = await axios.get(this.url + this.url_type);
-        let tempTypes = response.data.results;
-        tempTypes = tempTypes.map((type) => ({ ...type, checked: false }));
-        this.types = tempTypes;
-        console.log("types:", this.types);
-      } catch (err) {
-        console.error(err);
-      }
-    },
-    async caricaRisultati() {
-      const slug = this.handleSlug();
 
-      await this.getAllRestaurants(slug, 1);
-    },
-
-    async getAllRestaurants(slug, page = 1) {
-      try {
-        const response = await axios.get(
-          `${this.url}${this.url_restaurant}?page=${page}`
-        );
-        const allRestaurants = response.data.results.data;
-
-        const filteredRestaurants = allRestaurants.filter(
-          (restaurant) =>
-            restaurant.types.some((type) =>
-              this.selectedTypes.includes(type.slug)
-            ) && !this.selectedRestaurants.some((r) => r.id === restaurant.id)
-        );
-
-        this.selectedRestaurants = [
-          ...this.selectedRestaurants,
-          ...filteredRestaurants,
-        ];
-
-        const nextPage = response.data.results.next_page_url;
-        if (nextPage) {
-          const nextPageNumber = new URL(nextPage).searchParams.get("page");
-          await this.getAllRestaurants(slug, nextPageNumber);
-        }
-      } catch (error) {
-        console.error("Errore nella richiesta dei ristoranti:", error);
-      }
-    },
-    async loadPage(url) {
-      if (url) {
-        const pageNumber = new URL(url).searchParams.get("page");
-        await this.getAllRestaurants(pageNumber);
-      }
-    },
-    getSlug(index) {
-      const updatedTypes = this.types.map((type, i) => {
-        if (i === index) {
-          const typeGiaPresente = this.selectedTypes.find(
-            (el) => el === type.slug
-          );
-          if (typeGiaPresente) {
-            let indice;
-            this.selectedTypes.forEach((type, id) => {
-              if (type === typeGiaPresente) {
-                indice = id;
-              }
-            });
-            this.selectedTypes.splice(indice, 1);
-            this.selectedRestaurants = this.selectedRestaurants.filter(
-              (restaurant) =>
-                !restaurant.types.some((t) => t.slug === type.slug)
-            );
-          } else {
-            this.selectedTypes.push(type.slug);
-          }
-          return { ...type, checked: !type.checked };
-        } else {
-          return type;
-        }
-      });
-      this.types = updatedTypes;
-    },
-    handleSlug() {
-      if (this.selectedTypes.length < 2) {
-        return this.selectedTypes[0];
-      } else {
-        let finalUrl = "";
-
-        for (let index = 0; index < this.selectedTypes.length; index++) {
-          finalUrl = `${finalUrl}/${this.selectedTypes[index]}`;
-          console.log("finalUrl", finalUrl);
-        }
-        console.log("finalUrl:", finalUrl.slice(1));
-        return finalUrl.slice(1);
-      }
+    filterRestaurantsByTypes(url) {
+      axios
+        .get(url)
+        .then((response) => {
+          //console.log(response, "RESPONSE");
+          this.selectedRestaurants = response.data.results.data;
+          this.links = response.data.results.links;
+          this.maxPages = response.data.results.last_page;
+          this.currentPage = response.data.results.current_page;
+          //console.log(this.selectedTypes, "SELECTED TYPES");
+          //console.log(this.selectedRestaurants, "SELECTED RESTAURANTS");
+        })
+        .catch((error) => {
+          console.error("Error filtering restaurants by types", error);
+        });
     },
 
     getImageUrl(coverImage) {
@@ -132,31 +61,9 @@ export default {
         return "http://127.0.0.1:8000/storage/" + coverImage;
       }
     },
-    /*    nextPage() {
-      if (this.selectedRestaurants.next_page_url) {
-        const nextPageNumber = new URL(
-          this.selectedRestaurants.next_page_url
-        ).searchParams.get("page");
-        this.getAllRestaurants(nextPageNumber);
-      }
-    },
-
-    prevPage() {
-      if (this.selectedRestaurants.prev_page_url) {
-        const prevPageNumber = new URL(
-          this.selectedRestaurants.prev_page_url
-        ).searchParams.get("page");
-        this.getAllRestaurants(prevPageNumber);
-      }
-    },
-
-    setPage(page) {
-      this.getAllRestaurants(page);
-    }, */
   },
   mounted() {
-    /*     this.getType(this.url + this.url_type); */
-    this.fetchData();
+    this.fetchTypes();
   },
 };
 </script>
@@ -170,32 +77,48 @@ export default {
         <div
           class="row row-cols-1 row-cols-sm-3 row-cols-md-5 row-cols-lg-5 gap-4 justify-content-center pt-5"
         >
-          <div
-            class="form-check img_check img_ita bor_color d-flex justify-content-center gap-2 px-5 py-3"
-            v-for="(typ, index) in this.types"
-            :key="typ.id"
+          <label
+            class="form-check checkbox_elements bor_color d-flex justify-content-center gap-2 px-5 py-3"
+            v-for="typology in this.types"
+            :key="typology.id"
           >
             <input
               class="form-check-input"
               type="checkbox"
-              :value="typ.checked"
-              :id="typ.slug"
-              @change="getSlug(index)"
+              v-model="selectedTypes"
+              :value="typology.id"
             />
-            <label class="hover_effect custom-control-label" :for="typ.slug">
-              {{ typ.name }}
-            </label>
-          </div>
+            <span
+              class="hover_effect custom-control-label"
+              :for="typology.slug"
+            >
+              {{ typology.name }}
+            </span>
+          </label>
         </div>
         <div class="d-flex justify-content-center pt-5">
           <div class="d-flex justify-content-center pt-5">
-            <button @click="caricaRisultati()">Carica Risultati</button>
+            <button
+              class="button_primary"
+              @click="
+                filterRestaurantsByTypes(
+                  this.url +
+                    `/api/restaurants/types?type_ids=${this.selectedTypes.join(
+                      ','
+                    )}`
+                )
+              "
+            >
+              Carica Risultati
+            </button>
           </div>
         </div>
 
         <div class="container my-5">
           <div class="row text-center">
-            <h2>I nostri ristoranti</h2>
+            <h2 v-if="this.selectedRestaurants.length > 0">
+              I nostri ristoranti
+            </h2>
             <div
               class="col-md-4 d-flex mb-4 mt-4"
               v-for="restaurant in selectedRestaurants"
@@ -214,7 +137,7 @@ export default {
                   <p class="card-text">
                     <font-awesome-icon
                       icon="fa-solid fa-location-dot"
-                      style="color: #3d348b;"
+                      style="color: #3d348b"
                     />
                     {{ restaurant.address }}
                   </p>
@@ -231,19 +154,20 @@ export default {
             </div>
           </div>
         </div>
-        <nav aria-label="Page navigation" class="my-3 d-inline-block">
-          <ul class="pagination">
+        <nav aria-label="Page navigation" class="d-flex justify-content-center">
+          <ul class="pagination mt-5">
             <li
               class="page-item"
-              v-for="page in pageLinks"
-              :key="page.label"
-              :class="{
-                active: page.label == currentPage,
-                disabled: page.url === null,
-              }"
+              v-for="link in this.links"
+              :class="link.active ? 'active' : ''"
             >
-              <a class="page-link" href="#" @click.prevent="loadPage(page.url)">
-                {{ page.label }}
+              <a
+                class="page-link pf_bg_dark"
+                role="button"
+                aria-label="Previous"
+                v-html="link.label"
+                @click="filterRestaurantsByTypes(link.url)"
+              >
               </a>
             </li>
           </ul>
