df896afa28c6d29f4caa32984f09607dcf4c4ade
danielplebani <danielpleba03@gmail.com>
2023-12-12 19:30:30 +0100

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,31 +7,6 @@ export default {
 
 
 <template>
-
-    <header class="bg_color navbar py-3 px-1 p-sm-3 p-md-1 shadow">
-        <div class="container">
-            <div class="d-none d-md-flex align-items-center">
-                <img style="width: 80px" src="/src/assets/img/wired-lineal-13-pizza.gif" alt="pizza">
-                <img style="width: 200px" src="/src/assets/img/Screenshot_2023-12-09_153609-removebg-preview.png"
-                    alt="logo">
-            </div>
-            <div class="d-flex gap-3">
-                <button class="d-none d-sm-block btn header-button">
-                    <a class="col_select text-decoration-none" href="/">
-                        <i class="fa-solid fa-house"></i> Home
-                    </a>
-                </button>
-
-                <button class="btn header-registration-button">
-                    <a class="col_white text-decoration-none" href="http://127.0.0.1:8000/login">
-                        <i class="fa-solid fa-user"></i> Sei un ristoratore?
-                    </a>
-                </button>
-            </div>
-        </div>
-    </header>
-
-
     <div class="my_jumbo jumbotron jumbotron-fluid pt-5 pb-5">
 
 
@@ -106,73 +81,23 @@ export default {
 
 
     </div>
-    <svg id="wave" style="transform:rotate(180deg); transition: 0.3s" viewBox="0 0 1440 100" version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0"><stop stop-color="rgba(247, 184, 1, 1)" offset="0%"></stop><stop stop-color="rgba(255, 189.695, 0, 1)" offset="100%"></stop></linearGradient></defs><path style="transform:translate(0, 0px); opacity:1" fill="url(#sw-gradient-0)" d="M0,70L30,65C60,60,120,50,180,46.7C240,43,300,47,360,41.7C420,37,480,23,540,20C600,17,660,23,720,33.3C780,43,840,57,900,53.3C960,50,1020,30,1080,20C1140,10,1200,10,1260,21.7C1320,33,1380,57,1440,55C1500,53,1560,27,1620,28.3C1680,30,1740,60,1800,75C1860,90,1920,90,1980,88.3C2040,87,2100,83,2160,71.7C2220,60,2280,40,2340,28.3C2400,17,2460,13,2520,21.7C2580,30,2640,50,2700,50C2760,50,2820,30,2880,31.7C2940,33,3000,57,3060,70C3120,83,3180,87,3240,88.3C3300,90,3360,90,3420,83.3C3480,77,3540,63,3600,60C3660,57,3720,63,3780,65C3840,67,3900,63,3960,55C4020,47,4080,33,4140,36.7C4200,40,4260,60,4290,70L4320,80L4320,100L4290,100C4260,100,4200,100,4140,100C4080,100,4020,100,3960,100C3900,100,3840,100,3780,100C3720,100,3660,100,3600,100C3540,100,3480,100,3420,100C3360,100,3300,100,3240,100C3180,100,3120,100,3060,100C3000,100,2940,100,2880,100C2820,100,2760,100,2700,100C2640,100,2580,100,2520,100C2460,100,2400,100,2340,100C2280,100,2220,100,2160,100C2100,100,2040,100,1980,100C1920,100,1860,100,1800,100C1740,100,1680,100,1620,100C1560,100,1500,100,1440,100C1380,100,1320,100,1260,100C1200,100,1140,100,1080,100C1020,100,960,100,900,100C840,100,780,100,720,100C660,100,600,100,540,100C480,100,420,100,360,100C300,100,240,100,180,100C120,100,60,100,30,100L0,100Z"></path></svg>
+    <svg id="wave" style="transform:rotate(180deg); transition: 0.3s" viewBox="0 0 1440 100" version="1.1"
+        xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+                <stop stop-color="rgba(247, 184, 1, 1)" offset="0%"></stop>
+                <stop stop-color="rgba(255, 189.695, 0, 1)" offset="100%"></stop>
+            </linearGradient>
+        </defs>
+        <path style="transform:translate(0, 0px); opacity:1" fill="url(#sw-gradient-0)"
+            d="M0,70L30,65C60,60,120,50,180,46.7C240,43,300,47,360,41.7C420,37,480,23,540,20C600,17,660,23,720,33.3C780,43,840,57,900,53.3C960,50,1020,30,1080,20C1140,10,1200,10,1260,21.7C1320,33,1380,57,1440,55C1500,53,1560,27,1620,28.3C1680,30,1740,60,1800,75C1860,90,1920,90,1980,88.3C2040,87,2100,83,2160,71.7C2220,60,2280,40,2340,28.3C2400,17,2460,13,2520,21.7C2580,30,2640,50,2700,50C2760,50,2820,30,2880,31.7C2940,33,3000,57,3060,70C3120,83,3180,87,3240,88.3C3300,90,3360,90,3420,83.3C3480,77,3540,63,3600,60C3660,57,3720,63,3780,65C3840,67,3900,63,3960,55C4020,47,4080,33,4140,36.7C4200,40,4260,60,4290,70L4320,80L4320,100L4290,100C4260,100,4200,100,4140,100C4080,100,4020,100,3960,100C3900,100,3840,100,3780,100C3720,100,3660,100,3600,100C3540,100,3480,100,3420,100C3360,100,3300,100,3240,100C3180,100,3120,100,3060,100C3000,100,2940,100,2880,100C2820,100,2760,100,2700,100C2640,100,2580,100,2520,100C2460,100,2400,100,2340,100C2280,100,2220,100,2160,100C2100,100,2040,100,1980,100C1920,100,1860,100,1800,100C1740,100,1680,100,1620,100C1560,100,1500,100,1440,100C1380,100,1320,100,1260,100C1200,100,1140,100,1080,100C1020,100,960,100,900,100C840,100,780,100,720,100C660,100,600,100,540,100C480,100,420,100,360,100C300,100,240,100,180,100C120,100,60,100,30,100L0,100Z">
+        </path>
+    </svg>
 </template>
 
 
 <style lang="scss" scoped>
 @use "../assets/scss/variables" as *;
-
-.header-button {
-    padding: 9px 20px;
-    border-radius: 50px;
-    cursor: pointer;
-    border: 0;
-    background-color: white;
-    box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
-    letter-spacing: 1.5px;
-    text-transform: uppercase;
-    font-size: 15px;
-    transition: all 0.5s ease;
-
-    .col_select,
-    i {
-        color: #3d348b;
-        font-weight: bold;
-    }
-}
-
-.header-button:hover {
-    letter-spacing: 3px;
-    background-color: #3d348b;
-    box-shadow: rgb(93 24 220) 0px 7px 29px 0px;
-
-    .col_select,
-    i {
-        color: white;
-        font-weight: bold;
-    }
-}
-
-.header-registration-button {
-    padding: 9px 20px;
-    border-radius: 50px;
-    cursor: pointer;
-    border: 0;
-    background-color: #3d348b;
-    box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
-    letter-spacing: 1.5px;
-    text-transform: uppercase;
-    font-size: 15px;
-    transition: all 0.5s ease;
-
-    .col_white {
-        color: white;
-    }
-}
-
-.header-registration-button:hover {
-    letter-spacing: 3px;
-    background-color: white;
-    box-shadow: rgb(93 24 220) 0px 7px 29px 0px;
-
-    .col_white,
-    i {
-        color: #3d348b;
-        font-weight: bold;
-    }
-}
-
 
 @mixin white-gradient {
     background: linear-gradient(to right, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 100%);
