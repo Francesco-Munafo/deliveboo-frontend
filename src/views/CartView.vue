9f8ad82a21aa016c210d0e953724849babea337c
danielplebani <danielpleba03@gmail.com>
2023-12-15 11:24:18 +0100

    add a condition when no restaurants are found add total restaurants found count

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -45,9 +45,7 @@ export default {
 
 
 <template>
-
-
-<div class="container my-5">
+  <div class="container my-5">
     <h1 class="mb-4">Carrello</h1>
     <div class="table-responsive">
       <table class="table">
@@ -63,15 +61,12 @@ export default {
           </tr>
         </thead>
         <tbody>
-     
+
           <tr>
             <td>
-             
-            
-                <div class="media-body">
-                  <h5 class="mt-0">Nome Prodotto</h5>
-                </div>
-            
+              <div class="media-body">
+                <h5 class="mt-0">Nome Prodotto</h5>
+              </div>
             </td>
             <td>Taglia</td>
             <td>Colore</td>
@@ -87,7 +82,7 @@ export default {
         </tbody>
       </table>
     </div>
-    
+
     <div class="row">
       <div class="col-md-4 order-md-2 mb-4">
         <h4 class="d-flex justify-content-between align-items-center mb-3">
@@ -109,15 +104,12 @@ export default {
           </li>
         </ul>
 
-        
+
         <button class="btn btn-primary btn-lg btn-block" type="submit">Procedi al Checkout</button>
       </div>
       <div class="col-md-8 order-md-1">
         <button class="btn btn-link btn-sm">Continua lo shopping</button>
-       
       </div>
     </div>
   </div>
-
-
 </template>
