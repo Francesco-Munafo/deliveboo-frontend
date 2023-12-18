356743f8ba588c689d91bef41cb3d1b967b4ff9d
Francesco-Munafo <acquamarea1@gmail.com>
2023-12-18 13:56:43 +0100

    Add payment calls

--- store.js
+++ store.js
@@ -7,10 +7,14 @@ export const store = reactive({
   savedCart: localStorage.getItem("storageCart"),
   savedTotal: localStorage.getItem("storageTotalPrice"),
   totalPrice: 0,
+
+  //Orders user's info
   username: "",
   address: "",
   user_email: "",
   phone: "",
+  notes: "",
+
   saveCartToLocalStorage() {
     localStorage.setItem("storageCart", JSON.stringify(this.cart));
   },
@@ -23,7 +27,7 @@ export const store = reactive({
     if (store.savedCart) {
       store.cart = JSON.parse(store.savedCart);
     }
-  
+
     // Carica il prezzo totale dal localStorage quando il componente viene creato
     if (store.savedTotal) {
       store.totalPrice = JSON.parse(store.savedTotal);
@@ -33,7 +37,7 @@ export const store = reactive({
   beforeDestroy() {
     // Salva il carrello nel localStorage prima che il componente venga distrutto
     store.saveCartToLocalStorage();
-  
+
     // Salva il prezzo totale nel localStorage prima che il componente venga distrutto
     store.saveTotalPrice();
   },
